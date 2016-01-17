c893c7b0dc32e43d86b2a960cf69f89eb7623253
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2016-01-17 23:43:01 +0200

    Add Google Mock library

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
+#include "sdl_utils.hpp"
 
 int main(int argc, char* argv[]) {
   std::cout << "This works" << std::endl;
+  logSDLError(std::cout, "iitest");
   return 0;
 }
